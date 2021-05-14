16e5cfe78daa2056f3cf1284d406b99f4e071456
maslyankov <m.maslyankov@me.com>
2021-05-14 14:13:07 +0300

    Added more methods for lights control, optimized many things in the code, added data verification to setters:

--- main.py
+++ main.py
@@ -24,25 +24,69 @@ class TestLights:
 
         self.measurements = list()
 
-    def grab_data(self, color):
+    def grab_data(self, color=None, hsb=None):
         data = self.sensor.get('delta_uv')
+        if data[1] < 0:
+            return -1
 
-        self.measurements.append({
-            "color_hex": color,
+        new_data = {
             "EV": data[0],
             "TCP": data[1],
             "deltaUV": data[2]
-        })
+        }
 
-    def cycle_colors(self, action):
-        self.light.set_brightness(90)
+        if color:
+            new_data["color_hex"] = color
+        if hsb:
+            new_data["hsb"] = hsb
 
+        print(f"Measured: {new_data}")
+        self.measurements.append(new_data)
+
+    def go_through_colors_list(self, action=None, colors_list: list):
         test_counter = 0
+        colors_num = len(colors_list)
 
-        for red in range(0, 255):
-            for green in range(0, 255):
-                for blue in range(1, 255):
-                    if test_counter > 10:
+        print()
+        print("------------------------------")
+        print(f"Starting colors list... ({colors_num} tests)")
+
+        for color in colors_list:
+            print(f"Setting color {color}")
+
+            self.light.set_color(color)
+            print("Waiting 1.5 sec for the sensor to load...")
+            sleep(1.5)
+
+            # After change do...
+            res = 1
+            if action and self.sensor:
+                res = action(color=color)
+
+            if res != -1:
+                test_counter += 1
+                print(f"Test {test_counter}/{colors_num} saved")
+            else:
+                print("Results are not logical! Not counting this test!")
+
+            sleep(.5)
+
+        print("List ended!")
+        print("------------------------------")
+
+    def cycle_colors(self, action=None):
+        # self.light.set_brightness(100)
+        test_counter = 0
+        tests_num = 0  # 0 for infinite
+
+        print()
+        print("------------------------------")
+        print(f"Starting colors cycle... ({tests_num-1 if tests_num else 'infinite'} tests)")
+
+        for blue in range(150, 255):
+            for green in range(150, 255):
+                for red in range(150, 255):
+                    if tests_num != 0 and test_counter > tests_num:
                         break
 
                     red_hex = format(red, 'X').ljust(2, '0')
@@ -51,24 +95,78 @@ class TestLights:
 
                     color_hex = f"{red_hex}{green_hex}{blue_hex}"
 
-                    if blue % 9 == 0:
-                        print(f"Setting color {color_hex}")
+                    # if blue % 9 == 0:
+                    print(f"Setting color {color_hex}")
 
-                        self.light.set_color(color_hex)
-                        sleep(.5)
+                    self.light.set_color(color_hex)
+                    print("Waiting 1.5 sec for the sensor to load...")
+                    sleep(1.5)
 
-                        # After change do...
-                        if action and self.sensor:
-                            action(color=color_hex)
+                    # After change do...
+                    res = 1
+                    if action and self.sensor:
+                        res = action(color=color_hex)
 
+                    if res != -1:
                         test_counter += 1
-                        sleep(.5)
+
+                        print(f"Test {test_counter}/{tests_num} saved")
                     else:
-                        print(f"Skipping {color_hex}...")
+                        print("Results are not logical! Not counting this test!")
+
+                    sleep(.5)
+                    # else:
+                    #     print(f"Skipping {color_hex}...")
+
+        print("Cycling ended!")
+        print("------------------------------")
+
+    def cycle_hsb(self, action=None):
+        test_counter = 0
+        tests_num = 0  # 0 for infinite
+
+        print()
+        print("------------------------------")
+        print(f"Starting colors cycle... ({tests_num-1 if tests_num else 'all'} tests)")
+
+        target_brightness = 100
+
+        for hue in range(0, 100):
+            for saturation in range(0, 100):
+                if tests_num != 0 and test_counter > tests_num:
+                    break
+
+                hsb = [hue, saturation, target_brightness]
+
+                # if blue % 9 == 0:
+                print(f"Setting hsb to [{hue}, {saturation}, {target_brightness}]")
+
+                self.light.set_hsb(hsb)
+                print("Waiting 1.5 sec for the sensor to load...")
+                sleep(1.5)
+
+                # After change do...
+                res = 1
+                if action and self.sensor:
+                    res = action(hsb=hsb)
+
+                if res != -1:
+                    test_counter += 1
+
+                    print(f"Test {test_counter}/{tests_num} saved")
+                else:
+                    print("Results are not logical! Not counting this test!")
+
+                sleep(.5)
+
+        print("Cycling ended!")
+        print("------------------------------")
 
     def __del__(self):
         print("exiting...")
-        print(f"Measured data: {self.measurements}")
+        print(f"Measured data: \n{self.measurements}")
+        measured_colors = [d['color_hex'] for d in self.measurements if 'color_hex' in d]
+        print(f"colors list: \n{measured_colors}")
 
 
 def main():
@@ -76,36 +174,18 @@ def main():
 
     light = NeoLightsCtl()
 
+    light.set_brightness(100)
+
     s = Sensor(model="cl200a")
 
     print(s.get('all'))
 
     testing = TestLights(sensor=s, light=light)
 
-    try:
-        testing.cycle_colors(testing.grab_data)
-    except KeyboardInterrupt:
-        exit(0)
+    colors_list = ['969696', 'D79696', 'D89696', 'D99696', 'DA9696', 'DB9696', 'DC9696', 'DD9696', 'DE9696', 'DF9696', 'E09696', 'E19696', 'E29696', 'E39696', 'E49696', 'E59696', 'E69696', 'E79696', 'E89696', 'E99696', 'EA9696']
+    testing.go_through_colors_list(testing.grab_data, colors_list)
 
-    # for x in range(0, 5):
-    #     lights_ctl.set_brightness(20 * x)
-    #     sleep(1)
-    #
-    # lights_ctl.set_brightness(0)
-
-    # some_color = '009000'
-    #
-    # for pixel in range(lights_ctl.pixels_count, 0, -1):
-    #     print(f"setting pixel {pixel}")
-    #     lights_ctl.set_led(pixel, some_color)
-    #     sleep(1.5)
-
-    # Cycle colors
-    # lights_ctl.set_brightness(0)
-
-    # lights_ctl.set_brightness(60)
-
-    # cycle_colors(lights_ctl)
+    # testing.cycle_colors(testing.grab_data)
 
 
 if __name__ == "__main__":
--- neolights_ctl.py
+++ neolights_ctl.py
@@ -4,6 +4,7 @@ from json import loads
 
 from luxmeters.serial_utils import list_ports, find_all_luxmeters
 
+
 # from vendor.luxmeters.konica.CL200A import CL200A
 
 
@@ -35,7 +36,6 @@ class NeoLightsCtl:
             print(f"Found device at: {device}")
         else:
             raise ValueError("Could not find the serial port of lights...")
-            exit(1)
 
         baud = 115200
         try:
@@ -49,13 +49,17 @@ class NeoLightsCtl:
         # self.set_brightness(20)
 
         self.pixels_count = self.get_pixels_count()
-        print(f"Init pixels count = {self.pixels_count}")
 
-        self.dimmer = self.get_brightness()
-        print(f"Init dimmer = {self.dimmer}")
+        resp_data = self.get_colors()
+        self.power = bool(1 if resp_data['POWER'] == "ON" else 0)
+        self.color = resp_data['Color']
 
-        self.color = self.get_color()
-        print(f"Init color = {self.color}")
+        hsb = resp_data['HSBColor'].split(",")
+        self.hue = int(hsb[0])
+        self.saturation = int(hsb[1])
+        self.brightness = int(hsb[3])
+
+        self.channels = resp_data['Channel']
 
         # Turn Off Wifi
         self.send_cmd("Wifi 0")
@@ -63,32 +67,110 @@ class NeoLightsCtl:
     def read(self):
         return self.conn.read(self.conn.in_waiting).decode('ascii')
 
-    def send_cmd(self, cmd):
+    def send_cmd(self, cmd: str):
         self.conn.write(f"{cmd}\n".encode())
 
-    def get_pixels_count(self):
+    def send_cmds(self, *cmd_list):
+        # When sending multiple commands at once, you might get multiple lines of response
+        cmd_str = "Backlog "
+        for cmd in cmd_list:
+            cmd_str += f"{cmd} ;"
+
+        print("Executing multiple commands:")
+        print(cmd_str)
+
+        self.send_cmd(cmd_str)
+
+    def get_pixels_count(self) -> int:
         self.send_cmd("Pixels")
         # sleep(2)
-        return read_response(self)['Pixels']
+        return int(read_response(self)['Pixels'])
 
-    def set_led(self, led_num, color):
+    def get_colors(self) -> dict:
+        self.send_cmd("Color")
+        return read_response(self)
+
+    def set_led(self, led_num: int, color):
         self.send_cmd(f"Led{led_num} {color}")
 
-    def set_brightness(self, brightness):
-        self.dimmer = brightness
+    def set_brightness(self, brightness: int):
+        if not isinstance(brightness, int) or brightness < 0 or brightness > 100:
+            raise ValueError("Brightness value must be an integer between 0 and 100!")
+
+        self.brightness = brightness
         self.send_cmd(f"Dimmer {brightness}")
 
-    def get_brightness(self):
-        self.send_cmd("Dimmer")
-        return read_response(self)['Dimmer']
+    def get_brightness(self, new_read: bool = False):
+        if new_read:
+            self.send_cmd("Dimmer")
+            return read_response(self)['Dimmer']
+        return self.brightness
 
-    def set_color(self, color):
+    def set_color(self, color: str):
         self.color = color
         self.send_cmd(f"Color {color}")
 
-    def get_color(self):
-        self.send_cmd("Color")
-        return read_response(self)['Color']
+    def get_color(self, new_read: bool = False):
+        if new_read:
+            self.send_cmd("Color")
+            return read_response(self)['Color']
+        return self.color
+
+    def set_channels(self, r: int, g: int, b: int):
+        if \
+                not isinstance(r, int) or r < 0 or r > 100 \
+                        or not isinstance(g, int) or g < 0 or g > 100 \
+                        or not isinstance(b, int) or b < 0 or b > 100:
+            raise ValueError("Channel values must be integers between 0 and 100!")
+
+        self.send_cmds(f"Channel1 {r}", f"Channel2 {g}", f"Channel3 {b}")
+        self.channels = [r, g, b]
+
+    # HueSaturationBrightness
+    def set_hsb(self, hsb: list):
+        if \
+                not isinstance(hsb[0], int) or hsb[0] < 0 or hsb[0] > 100 \
+                        or not isinstance(hsb[1], int) or hsb[1] < 0 or hsb[1] > 100 \
+                        or not isinstance(hsb[2], int) or hsb[2] < 0 or hsb[2] > 100:
+            raise ValueError("HSB values must be integers between 0 and 100!")
+
+        self.hue = hsb[0]
+        self.saturation = hsb[1]
+        self.brightness = hsb[2]
+
+        self.send_cmd(f"HSBColor {hsb}")
+
+        resp = read_response(self)
+        self.save_resp_data(resp)
+
+    # HueSaturationBrightness
+    def get_hsb(self, new_read: bool = False):
+        if new_read:
+            self.send_cmd("HSBColor")
+            resp = read_response(self)
+
+            self.save_resp_data(resp)
+            return resp['HSBColor']
+        return self.hue, self.saturation, self.brightness
+
+    def save_resp_data(self, data: dict):
+        try:
+            self.power = bool(1 if data['POWER'] == "ON" else 0)
+        except KeyError:
+            return
+
+        try:
+            self.color = data['Color']
+        except KeyError:
+            return
+
+        try:
+            hsb = data['HSBColor'].split(",")
+            self.hue = int(hsb[0])
+            self.saturation = int(hsb[1])
+            self.brightness = int(hsb[3])
+        except KeyError:
+            return
 
     def __del__(self):
         self.set_brightness(0)
